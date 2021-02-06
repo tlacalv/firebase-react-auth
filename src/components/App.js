b34eb5f9c1574e07876f2354e504f1543dd34e62
Tlacaelel <radeon777@live.com>
2021-02-06 16:51:10 -0700

    Created dashboar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Signup from './Signup.js';
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center"
-      style={{minHeight: "100vh"}}
-      >
-        <dvi className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </dvi>
-      </Container>
-    </AuthProvider>
-  
+    <Container className="d-flex align-items-center justify-content-center"
+    style={{minHeight: "100vh"}}
+    >
+      <dvi className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </dvi>
+    </Container>
+
   )
 }
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,6 +14,9 @@ export function AuthProvider({ children }) {
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password)
   }
+  function  login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
   useEffect(() => {
     const unsuscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user)
@@ -25,6 +28,7 @@ export function AuthProvider({ children }) {
   
   const value = {
     currentUser,
+    login,
     signup
   }
   return (
